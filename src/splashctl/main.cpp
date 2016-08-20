76fd4c2b8c89337c548897000b7603ef008a31c2
Andrew Zonenberg <azonenberg@drawersteak.com>
2016-08-20 12:40:55 -0700

    Initial commit of splashctl server

---
+++ src/splashctl/main.cpp
@@ -0,0 +1,104 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* SPLASH build system v0.2                                                                                             *
+*                                                                                                                      *
+* Copyright (c) 2016 Andrew D. Zonenberg                                                                               *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+#include "splashctl.h"
+
+using namespace std;
+
+void ShowUsage();
+void ShowVersion();
+
+/**
+	@brief Program entry point
+ */
+int main(int argc, char* argv[])
+{
+	int port = 49000;
+
+	LogSink::Severity console_verbosity = LogSink::NOTICE;
+	
+	//Parse command-line arguments
+	for(int i=1; i<argc; i++)
+	{
+		string s(argv[i]);
+		
+		//Let the logger eat its args first
+		if(ParseLoggerArguments(i, argc, argv, console_verbosity))
+			continue;
+		
+		else if(s == "--help")
+		{
+			ShowUsage();
+			return 0;
+		}
+		
+		else if(s == "--version")
+		{
+			ShowVersion();
+			return 0;
+		}
+		
+		//Last arg without a switch is the port number
+		//TODO: mandatory arguments to introduce this?
+		//TODO: sanity check that it's numeric
+		else
+			port = atoi(argv[i]);
+
+	}
+	
+	//Set up logging
+	g_log_sinks.emplace(g_log_sinks.begin(), new STDLogSink(console_verbosity));
+
+	//Print header
+	if(console_verbosity >= LogSink::NOTICE)
+	{
+		ShowVersion();
+		printf("\n");
+	}
+
+	//TODO: Run the actual server
+
+	return 0;
+}
+
+void ShowVersion()
+{
+	printf(
+		"SPLASH control server daemon by Andrew D. Zonenberg.\n"
+		"\n"
+		"License: 3-clause BSD\n"
+		"This is free software: you are free to change and redistribute it.\n"
+		"There is NO WARRANTY, to the extent permitted by law.\n");
+}
+
+void ShowUsage()
+{
+	printf("Usage: splashctl [control_port]\n");
+	exit(0);
+}
